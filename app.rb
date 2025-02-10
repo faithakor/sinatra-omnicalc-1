b8b3498dea492265cd6e911306b240f78da0579f
faithakor <fakor@chicagobooth.edu>
2025-02-10 22:38:46 +0000

    v3

--- app.rb
+++ app.rb
@@ -26,7 +26,44 @@ get "/random/new" do
 end
 
 get "/square/results" do
+  @calculator = "square"
   @number = params[:number].to_f
-  @square = @number ** 2
-  erb :results
+  @result = @number ** 2
+  erb :form
+end
+
+get "/square_root/results" do
+  @calculator = "square_root"
+  @number = params[:number].to_f
+  @result = Math.sqrt(@number)
+  erb :form
+end
+
+get "/payment/results" do
+  @calculator = "payment"
+  principal = params[:principal].to_f
+  apr = params[:apr].to_f
+  years = params[:years].to_f
+  
+  monthly_rate = (apr / 100) / 12
+  total_payments = years * 12
+  
+  if monthly_rate == 0
+    @monthly_payment = principal / total_payments
+  else
+    @monthly_payment = principal * (monthly_rate * (1 + monthly_rate)**total_payments) / ((1 + monthly_rate)**total_payments - 1)
+  end
+  
+  @formatted_payment = sprintf("$%.2f", @monthly_payment)
+  @formatted_apr = sprintf("%.4f%%", apr)
+  @formatted_principal = sprintf("$%s", principal.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse)
+  erb :form
+end
+
+get "/random/results" do
+  @calculator = "random"
+  min = params[:min].to_i
+  max = params[:max].to_i
+  @result = rand(min..max)
+  erb :form
 end
